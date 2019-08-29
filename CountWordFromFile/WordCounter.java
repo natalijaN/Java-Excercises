c49878fea1a09e881d702353c889bb92f4dd0151
Natalija Nikolova <45513903+natalijaN@users.noreply.github.com>
2019-08-29 14:44:03 +0200

    Updated code

--- CountWordFromFile/WordCounter.java
+++ CountWordFromFile/WordCounter.java
@@ -2,21 +2,23 @@ import java.io.File;
 import java.util.Scanner;
 import java.util.*;
 
-public class CountWordFile   
+public class WordCounter
 {  
     public static void main(String[] args) throws Exception  { 
 		File myFile = new File("data.txt");
 		Scanner input = new Scanner(myFile);
 		input.useDelimiter("(\\n)| |,");
 		
-		ArrayList<String> list = new ArrayList<String>();
+		List<String> wordsList = new ArrayList<String>();
+		
 		while(input.hasNext()){
 			String word = input.next();
-			list.add(word);
+			wordsList.add(word);
 		}
-		list.forEach(w -> System.out.println(w));
-		System.out.println(String.format("Number of words in the file: %d", list.size()));
-		long numOfUniqueWords = list.stream().distinct().count();
+		
+		wordsList.forEach(w -> System.out.println(w));
+		System.out.println(String.format("Number of words in the file: %d", wordsList.size()));
+		long numOfUniqueWords = wordsList.stream().distinct().count();
 		System.out.println(String.format("Number of unique words in the file: %d", numOfUniqueWords));
     }
 }
